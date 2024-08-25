32903336f69f8ba5d1114af116dec262edef76d5
wrat2 <bartekwrat@gmail.com>
2024-08-25 14:24:22 +0200

    Changed reading database from psycopg2 to sqlalchemy engine

--- webpage/models.py
+++ webpage/models.py
@@ -2,74 +2,53 @@ import os
 
 from dotenv import load_dotenv
 import pandas as pd
-import psycopg2
+from sqlalchemy import create_engine
 
 from utils import filter_by_range
 
+load_dotenv()
+DB_USER = os.getenv('DBUSER')
+DB_PASSWORD = os.getenv('DBPASSWORD')
+DB_NAME = os.getenv('DBNAME')
+DB_HOST = os.getenv('DBHOST')
+DB_PORT = os.getenv('DBPORT')
+
+DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URI)
+
 
 def get_stock_values_from_db():
-    load_dotenv()
-    DB_USER = os.getenv('DBUSER')
-    DB_PASSWORD = os.getenv('DBPASSWORD')
-    DB_NAME = os.getenv('DBNAME')
-
     df = None
-
+    query = "SELECT ticker, date, close FROM stock_prize"
     try:
-        with psycopg2.connect(database=DB_NAME, user=DB_USER, password=DB_PASSWORD) as conn:
-            with conn.cursor() as cur:
-                query = "SELECT ticker, date, close FROM stock_prize"
-                sql = pd.read_sql_query(query, conn)
-                df = pd.DataFrame(sql, columns=["ticker", "date", "close"])
-
-                if df.empty:
-                    return None
-
-                df['date'] = pd.to_datetime(df['date'])
-
-                return df
-    except psycopg2.Error as e:
-        raise Exception(f"Database error: {e}")
+        with engine.connect() as conn:
+            df = pd.read_sql(sql=query, con=conn.connection)
+        return df
+    except Exception as e:
+        print(f"Database error: {e}")
+        return None
 
 
 def get_stock_companies_from_db():
-    load_dotenv()
-    DB_USER = os.getenv('DBUSER')
-    DB_PASSWORD = os.getenv('DBPASSWORD')
-    DB_NAME = os.getenv('DBNAME')
-
     df = None
-
+    query = "SELECT ticker, name FROM companies"
     try:
-        with psycopg2.connect(database=DB_NAME, user=DB_USER, password=DB_PASSWORD) as conn:
-            with conn.cursor() as cur:
-                query = "SELECT ticker, name FROM companies"
-                sql = pd.read_sql_query(query, conn)
-                df = pd.DataFrame(sql, columns=["ticker", "name"])
-                if df.empty:
-                    return None
-                else:
-                    return df
-    except psycopg2.Error as e:
-        raise Exception(f"Database error: {e}")
+        with engine.connect() as conn:
+            df = pd.read_sql(sql=query, con=conn.connection)
+        return df
+    except Exception as e:
+        print(f"Database error: {e}")
+        return None
 
 
 def get_last_update_from_db():
-    # TODO configure reading from db last_updated date
-    load_dotenv()
-    DB_USER = os.getenv('DBUSER')
-    DB_PASSWORD = os.getenv('DBPASSWORD')
-    DB_NAME = os.getenv('DBNAME')
-
+    df = None
+    query = "SELECT date, ticker FROM stock_prize ORDER BY date DESC LIMIT 1"
     try:
-        with psycopg2.connect(database=DB_NAME, user=DB_USER, password=DB_PASSWORD) as conn:
-            with conn.cursor() as cur:
-                query = "SELECT date, ticker FROM stock_prize ORDER BY date DESC LIMIT 1"
-                sql = pd.read_sql_query(query, conn)
-                df = pd.DataFrame(sql, columns=["date", "ticker"])
-            if df.empty:
-                return None
-            else:
-                return df
-    except psycopg2.Error as e:
-        raise Exception(f"Database error: {e}")
+        with engine.connect() as conn:
+            df = pd.read_sql(sql=query, con=conn.connection)
+        return df
+    except Exception as e:
+        print(f"Database error: {e}")
+        return None
