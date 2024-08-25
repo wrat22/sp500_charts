d67195800a50aa47b043aa022c8e1afa8917a9f7
wrat2 <bartekwrat@gmail.com>
2024-08-25 15:15:24 +0200

    Added docstrings for functions in api.py

--- webpage/api.py
+++ webpage/api.py
@@ -8,6 +8,19 @@ api_routes = Blueprint('api_routes', __name__)
 
 @api_routes.route('/api/stock<ticker>', methods=['GET'])
 def get_stock_data_api(ticker):
+    """
+        Api route handler for ('api/stock<ticker>') with GET method
+
+        Retrieves stock data for a specific ticker symbol and returns it in JSON format
+
+        :parameter:
+            - 'ticker' (str): the name of the company for which stock is requested.
+            - 'range' (str, optional): the data range for filtering the stock data.
+        :return:
+            Response: A Flask 'Response' object with a JSON payload containing:
+                - A list of dictionaries where each dictionary represents a record of
+                stock data with fields such as date, ticker, and price.
+    """
     range_option = request.args.get('range', default='all')
 
     pandas_companies_df = get_stock_companies_from_db()
@@ -27,6 +40,25 @@ def get_stock_data_api(ticker):
 
 @api_routes.route('/api/compare_stocks', methods=['GET'])
 def compare_stocks_api():
+    """
+        Api route handler for ('api/compare_stocks') with GET method
+
+        Compares stock data for two given tickers and return their historical stock prices
+        over a specified data range in JSON format.
+
+        :parameter:
+            - 'first_ticker' (str): the name of the first stock to compare.
+            - 'second_ticker' (str): the name of the second stock to compare.
+            - 'range' (str, optional): the data range for filtering the stock data. Defaults to 'all'.
+        :return:
+            Response: A Flask 'Response' object with a JSON payload containing:
+                - 'first_ticker' (str): The ticker symbol for the first stock.
+                - 'first_data' (list of dicts): A list of dictionaries where each dictionary represents.
+                a record of stock data for the first ticker.
+                - 'second_ticker' (str): The ticker symbol for the second stock.
+                - 'second_data' (list of dicts): A list of dictionaries where each dictionary represents.
+                a record of stock data for the second ticker.
+    """
     first_ticker = request.args.get('first_ticker')
     second_ticker = request.args.get('second_ticker')
     range_option = request.args.get('range', default='all')
